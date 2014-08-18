2dddb2becde53bb8c191c54e900550fd37b83ba8
Oscar Ekholm <i@oscarekholm.se>
2014-08-18 16:37:10 +0200

    init script now also moving dotfiles, as well as specifying bash for execution

--- init.sh
+++ init.sh
@@ -1,7 +1,14 @@
-mv app-engine-boilerplate/* ./
+#!/bin/bash
 
+# Remove .git folders to avoid conflicts when copying dotfiles
+rm -rf app-engine-boilerplate/.git agigen-frontend-boilerplate/.git
+
+# Move all files from the app engine boilerplate to this folder
+mv app-engine-boilerplate/* app-engine-boilerplate/.[^.]* ./
+
+# Create a frontend directory and move everything from agigen-frontend-boilerplate there
 mkdir frontend
-mv agigen-frontend-boilerplate/* frontend/
+mv agigen-frontend-boilerplate/* agigen-frontend-boilerplate/.[^.]* frontend/
 
 rm -rf app-engine-boilerplate agigen-frontend-boilerplate
 
