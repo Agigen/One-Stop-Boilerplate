f1468a59ccf1ccd2e0831e1be91c89aa6e8b9c3f
Johan Steffner <johan@agigen.se>
2014-11-19 09:03:14 +0100

    updated to use gulp etc

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Remove .git folders to avoid conflicts when copying dotfiles
-rm -rf app-engine-boilerplate/.git agigen-frontend-boilerplate/.git
+# Remove .git folders
+rm -rf app-engine-boilerplate/.git agigen-frontend-boilerplate/.git .git
 
 # Move all files from the app engine boilerplate to this folder
 mv app-engine-boilerplate/* app-engine-boilerplate/.[^.]* ./
@@ -13,18 +13,12 @@ mv agigen-frontend-boilerplate/* agigen-frontend-boilerplate/.[^.]* frontend/
 # Remove both submodule directories
 rm -rf app-engine-boilerplate agigen-frontend-boilerplate
 
-# Link package.json to make sure all npm modules are synced
-# when running grunt in the project's root folder
-ln -s frontend/package.json package.json
+# Move package.json
+mv frontend/package.json package.json
 
 # Install npm modules in the current directory
 npm install
-
-# Change directory to frontend and install npm modules and bower components
-cd frontend
-npm install
 bower install
-cd ..
 
 # Bye bye init.sh
 rm init.sh
