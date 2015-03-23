b608fd3d8bcfa04153d39e07329bf0a5f23c479a
Johan Steffner <johan@agigen.se>
2015-03-23 10:08:17 +0100

    updated init.sh

--- init.sh
+++ init.sh
@@ -3,33 +3,24 @@
 # Remove .git folders
 rm -rf app-engine-boilerplate/.git agigen-frontend-boilerplate/.git .git
 
-# Move all files from the app engine boilerplate to this folder
-mv app-engine-boilerplate/* app-engine-boilerplate/.[^.]* ./
+# Remove REAME files
+rm app-engine-boilerplate/README.md
+rm agigen-frontend-boilerplate/README.md
 
-# Create a frontend directory and move everything from agigen-frontend-boilerplate there
-mkdir frontend
-mv agigen-frontend-boilerplate/* agigen-frontend-boilerplate/.[^.]* frontend/
+# Move all files from the app-engine-boilerplate
+mv app-engine-boilerplate/* ./
+
+# Move all files from the agigen-frontend-boilerplate
+mv agigen-frontend-boilerplate/* ./
+
+# Concat ignore files
+cat app-engine-boilerplate/.gitignore > .gitignore
+cat agigen-frontend-boilerplate/.gitignore > .gitignore
+
+mv agigen-frontend-boilerplate/.jshintrc > .jshintrc
 
 # Remove both submodule directories
 rm -rf app-engine-boilerplate agigen-frontend-boilerplate
-
-# Move gulpfile.js
-mv frontend/gulpfile.js gulpfile.js
-
-# Move .jshintrc
-mv frontend/.jshintrc .jshintrc
-
-# Move bower.json
-mv frontend/bower.json bower.json
-
-# Move package.json
-mv frontend/package.json package.json
-
-# Remove old .bowerrc
-rm frontend/.bowerrc
-
-# New .bowerrc
-echo "{\"directory\": \"frontend/libs/bower_components\"}" > .bowerrc
 
 # Install npm modules in the current directory
 npm install
